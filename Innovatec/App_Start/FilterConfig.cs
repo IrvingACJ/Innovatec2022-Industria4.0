9a2cb944d47829986d08221611ee9ff475882380
Irving Juárez <Deltairving55@hotmail.com>
2022-02-20 10:18:30 -0700

    Agregar archivos de proyecto.

---
+++ Innovatec/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Innovatec
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}

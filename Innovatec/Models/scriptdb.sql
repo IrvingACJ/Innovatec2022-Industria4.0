fb8ca62124a32b9986e54bcc121ea53a776a1eed
Irving Juárez <Deltairving55@hotmail.com>
2022-05-12 14:53:28 -0700

    database

--- Innovatec/Models/scriptdb.sql
+++ Innovatec/Models/scriptdb.sql
@@ -1,12 +1,13 @@
 USE [master]
 GO
-/****** Object:  Database [SistemaI4]    Script Date: 3/14/2022 11:49:10 PM ******/
+/****** Object:  Database [SistemaI4]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 CREATE DATABASE [SistemaI4]
  CONTAINMENT = NONE
  ON  PRIMARY 
-( NAME = N'SistemaI4', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\SistemaI4.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+( NAME = N'SistemaI4', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\SistemaI4.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
  LOG ON 
-( NAME = N'SistemaI4_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\SistemaI4_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+( NAME = N'SistemaI4_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\SistemaI4_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
 ALTER DATABASE [SistemaI4] SET COMPATIBILITY_LEVEL = 140
 GO
@@ -73,59 +74,26 @@ ALTER DATABASE [SistemaI4] SET TARGET_RECOVERY_TIME = 60 SECONDS
 GO
 ALTER DATABASE [SistemaI4] SET DELAYED_DURABILITY = DISABLED 
 GO
+ALTER DATABASE [SistemaI4] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
 ALTER DATABASE [SistemaI4] SET QUERY_STORE = OFF
 GO
 USE [SistemaI4]
 GO
-ALTER DATABASE SCOPED CONFIGURATION SET IDENTITY_CACHE = ON;
+/****** Object:  User [Python]    Script Date: 12/05/2022 02:52:33 p. m. ******/
+CREATE USER [Python] FOR LOGIN [Python] WITH DEFAULT_SCHEMA=[dbo]
 GO
-ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+ALTER ROLE [db_datareader] ADD MEMBER [Python]
 GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
+ALTER ROLE [db_datawriter] ADD MEMBER [Python]
 GO
-ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET MAXDOP = PRIMARY;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET PARAMETER_SNIFFING = PRIMARY;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
-GO
-USE [SistemaI4]
-GO
-/****** Object:  User [SysfacWeiser1]    Script Date: 3/14/2022 11:49:10 PM ******/
-CREATE USER [SysfacWeiser1] FOR LOGIN [SysfacWeiser1] WITH DEFAULT_SCHEMA=[SysfacWeiser1]
-GO
-/****** Object:  User [itninnova]    Script Date: 3/14/2022 11:49:10 PM ******/
-CREATE USER [itninnova] FOR LOGIN [itninnova] WITH DEFAULT_SCHEMA=[itninnova]
-GO
-ALTER ROLE [db_ddladmin] ADD MEMBER [SysfacWeiser1]
-GO
-ALTER ROLE [db_backupoperator] ADD MEMBER [SysfacWeiser1]
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [SysfacWeiser1]
-GO
-ALTER ROLE [db_datawriter] ADD MEMBER [SysfacWeiser1]
-GO
-ALTER ROLE [db_ddladmin] ADD MEMBER [itninnova]
-GO
-ALTER ROLE [db_backupoperator] ADD MEMBER [itninnova]
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [itninnova]
-GO
-ALTER ROLE [db_datawriter] ADD MEMBER [itninnova]
-GO
-/****** Object:  Schema [itninnova]    Script Date: 3/14/2022 11:49:10 PM ******/
+/****** Object:  Schema [itninnova]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 CREATE SCHEMA [itninnova]
 GO
-/****** Object:  Schema [SysfacWeiser1]    Script Date: 3/14/2022 11:49:10 PM ******/
+/****** Object:  Schema [SysfacWeiser1]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 CREATE SCHEMA [SysfacWeiser1]
 GO
-/****** Object:  Table [dbo].[tBasculas]    Script Date: 3/14/2022 11:49:10 PM ******/
+/****** Object:  Table [dbo].[tBasculas]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -136,10 +104,10 @@ CREATE TABLE [dbo].[tBasculas](
  CONSTRAINT [PK_tBasculas] PRIMARY KEY CLUSTERED 
 (
 	[ID_Bascula] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tBOM]    Script Date: 3/14/2022 11:49:11 PM ******/
+/****** Object:  Table [dbo].[tBOM]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -152,10 +120,10 @@ CREATE TABLE [dbo].[tBOM](
  CONSTRAINT [PK_tBOM] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tComponentes]    Script Date: 3/14/2022 11:49:11 PM ******/
+/****** Object:  Table [dbo].[tComponentes]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -170,10 +138,10 @@ CREATE TABLE [dbo].[tComponentes](
  CONSTRAINT [PK_tComponentes] PRIMARY KEY CLUSTERED 
 (
 	[ID_Componente] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tInventario]    Script Date: 3/14/2022 11:49:11 PM ******/
+/****** Object:  Table [dbo].[tInventario]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -183,7 +151,7 @@ CREATE TABLE [dbo].[tInventario](
 	[ID_Bascula] [varchar](25) NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tLineasComponentesBasculas]    Script Date: 3/14/2022 11:49:11 PM ******/
+/****** Object:  Table [dbo].[tLineasComponentesBasculas]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -193,13 +161,16 @@ CREATE TABLE [dbo].[tLineasComponentesBasculas](
 	[ID_Linea] [varchar](25) NULL,
 	[ID_Componente] [varchar](25) NULL,
 	[ID_Bascula] [varchar](25) NULL,
+	[Cantidad] [int] NULL,
+	[Minimo] [int] NULL,
+	[Maximo] [int] NULL,
  CONSTRAINT [PK_tIneasComponentesBasculas] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tLineasProduccion]    Script Date: 3/14/2022 11:49:11 PM ******/
+/****** Object:  Table [dbo].[tLineasProduccion]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -210,10 +181,10 @@ CREATE TABLE [dbo].[tLineasProduccion](
  CONSTRAINT [PK_tLineasProduccion] PRIMARY KEY CLUSTERED 
 (
 	[ID_Linea] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tProductos]    Script Date: 3/14/2022 11:49:11 PM ******/
+/****** Object:  Table [dbo].[tProductos]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -225,10 +196,10 @@ CREATE TABLE [dbo].[tProductos](
  CONSTRAINT [PK_tProductos] PRIMARY KEY CLUSTERED 
 (
 	[ID_Producto] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[tUsuarios]    Script Date: 3/14/2022 11:49:11 PM ******/
+/****** Object:  Table [dbo].[tUsuarios]    Script Date: 12/05/2022 02:52:33 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -240,8 +211,28 @@ CREATE TABLE [dbo].[tUsuarios](
  CONSTRAINT [PK_tUsuarios] PRIMARY KEY CLUSTERED 
 (
 	[ID_Usuario] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+INSERT [dbo].[tBasculas] ([ID_Bascula], [Descripcion]) VALUES (N'0xb827ebeab6ff2324', N'Almacen:Active')
+GO
+INSERT [dbo].[tBasculas] ([ID_Bascula], [Descripcion]) VALUES (N'0xb827ebeab6ff56', N'Almacen:Active')
+GO
+INSERT [dbo].[tComponentes] ([ID_Componente], [Descripcion], [PesoUnitarioGramos], [Minimo], [Maximo], [Imagen]) VALUES (N'1', N'Componente_1', CAST(32.0000000 AS Decimal(18, 7)), CAST(2.0000000 AS Decimal(18, 7)), CAST(12.0000000 AS Decimal(18, 7)), NULL)
+GO
+INSERT [dbo].[tComponentes] ([ID_Componente], [Descripcion], [PesoUnitarioGramos], [Minimo], [Maximo], [Imagen]) VALUES (N'2', N'Componente_2', CAST(100.0000000 AS Decimal(18, 7)), CAST(2.0000000 AS Decimal(18, 7)), CAST(100.0000000 AS Decimal(18, 7)), NULL)
+GO
+SET IDENTITY_INSERT [dbo].[tLineasComponentesBasculas] ON 
+GO
+INSERT [dbo].[tLineasComponentesBasculas] ([ID], [ID_Linea], [ID_Componente], [ID_Bascula], [Cantidad], [Minimo], [Maximo]) VALUES (4, N'2', N'1', N'0xb827ebeab6ff56', 6, 6, 12)
+GO
+INSERT [dbo].[tLineasComponentesBasculas] ([ID], [ID_Linea], [ID_Componente], [ID_Bascula], [Cantidad], [Minimo], [Maximo]) VALUES (5, N'1', N'1', N'0xb827ebeab6ff2324', 5, 3, 12)
+GO
+SET IDENTITY_INSERT [dbo].[tLineasComponentesBasculas] OFF
+GO
+INSERT [dbo].[tLineasProduccion] ([ID_Linea], [Descripcion]) VALUES (N'1', N'Linea_1')
+GO
+INSERT [dbo].[tLineasProduccion] ([ID_Linea], [Descripcion]) VALUES (N'2', N'Linea_2')
 GO
 ALTER TABLE [dbo].[tBOM]  WITH CHECK ADD  CONSTRAINT [FK_tBOM_tComponentes] FOREIGN KEY([ID_Componente])
 REFERENCES [dbo].[tComponentes] ([ID_Componente])
